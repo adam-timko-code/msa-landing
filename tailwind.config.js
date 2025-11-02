e9ff0602abd535f4519493c6d2fe36dd528af425
adam-timko-code <atimko.at@gmail.com>
2025-11-02 11:42:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50:  "#EEF2FF",
+          100: "#E0E7FF",
+          200: "#C7D2FE",
+          400: "#A7F3D0",
+          500: "#34D399",
+          600: "#4F46E5",
+          700: "#4338CA",
+          900: "#312E81",
+        },
+      },
+    },
   },
   plugins: [],
 };
